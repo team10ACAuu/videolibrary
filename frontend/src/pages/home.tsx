e9c2a48e5d72c542a36ad2e3a6ed05e88180fd27
micjarda <33402682+micjarda@users.noreply.github.com>
2023-05-27 21:41:27 +0200

    Načítání videí na hlavní stránce 📲

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Card from "../components/card"
 
 const Home = () => {
     const [videosData, setVideosData] = useState({})
@@ -6,7 +7,7 @@ const Home = () => {
     useEffect(() => {
         const fetchVideoData = async () => {
             const response = await fetch('/api')
-            console.table(response);
+            //console.table(response);
             const json = await response.json()
             if(response.ok) {
                 setVideosData(json)
@@ -20,11 +21,29 @@ const Home = () => {
             console.log("Can't reach /")
         }
         fetchVideoData();
-    }, [])
-    console.log(videosData)
+    }, []);
+
+    const VIDEOS: object[] = [];
+    (Object.keys(videosData) as (keyof typeof videosData)[]).forEach((video, index) => {
+        let ATTRIBUTES: string[] = [];
+        Object.keys(videosData[video]).forEach(attribute => {
+            ATTRIBUTES.push(videosData[video][attribute]);
+        });
+        VIDEOS.push(
+            <Card
+            key={index}
+                thumbnail={ATTRIBUTES[13]}
+                title={ATTRIBUTES[3]}
+                description={ATTRIBUTES[5]}
+            />
+        );
+        ATTRIBUTES = [];
+    });
     return ( 
         <>
-            {videosData.toString}
+            {
+                VIDEOS
+            }
         </>
     );
 }
