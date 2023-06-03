956d81052125b28cdff0f3ac87ea34ce501a26f6
DavidRysanek <dawelich@gmail.com>
2023-06-03 14:18:04 +0200

    Responzivita u YouTube přehrávače

--- frontend/src/components/ytplayer.tsx
+++ frontend/src/components/ytplayer.tsx
@@ -11,11 +11,15 @@ interface Options {
 
 const YouTubePlayer: React.FC<YouTubePlayerProps> = ({ videoId }) => {
   const opts: Options = {
-    height: '280',
-    width: '420',
+    height: '100%',
+    width: '100%',
   };
 
-  return <YouTube videoId={videoId} opts={opts} />;
+  return (
+    <div style={{position: 'relative', height: '85vh', width: '100%', overflow: 'hidden'}}>
+      <YouTube videoId={videoId} opts={opts} style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}} />
+    </div>
+  );
 };
 
 export default YouTubePlayer;
--- frontend/src/pages/ControlPanel.tsx
+++ frontend/src/pages/ControlPanel.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Box, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
 
 import {
     Stack,
@@ -88,9 +89,27 @@ const ControlPanel = () => {
                 <Tab>Úpravy</Tab>
               </TabList>
               <TabPanels>
-                <TabPanel>
-                  <p>Vlastnosti</p>
-                </TabPanel>
+              <TabPanel>
+        <Box boxSize="small">
+        <img src="path/to/your/image.jpg" alt="Description" />
+        </Box>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Název</Th>
+              <Th>Popisek</Th>
+              <Th>Odkaz</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>{videosData[0].title}</Td>
+              <Td>{videosData[0].description}</Td>
+              <Td>{videosData[0].link}</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TabPanel>
                 <TabPanel>
                   <Editable onChange={setTitle} placeholder={videosData[0].title}>
                       <EditablePreview />
