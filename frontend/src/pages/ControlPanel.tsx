677791f1e3d43f0b72feb6188ff3af3700892994
micjarda <33402682+micjarda@users.noreply.github.com>
2023-06-04 09:41:45 +0200

    Zarovnání kódu v dashboard.

--- frontend/src/pages/ControlPanel.tsx
+++ frontend/src/pages/ControlPanel.tsx
@@ -2,12 +2,6 @@ import { useState, useRef } from 'react';
 import { Box, Link, AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay, } from "@chakra-ui/react";
 import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
-
-
-
-
-
-
 import {
   Stack,
   Input,
@@ -25,9 +19,7 @@ import {
   Text,
 } from '@chakra-ui/react';
 
-
 import { CheckIcon, SearchIcon } from '@chakra-ui/icons';
-
 
 const baseYoutubeUrl = "https://www.youtube.com/watch?v=";
 const placeholderImage = "/src/assets/images/pm.png";
@@ -37,13 +29,10 @@ const ControlPanel = () => {
   const [videoId, setVideoId] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [topic, setTopic] = useState("");
   const [link, setLink] = useState("");
   const [thumbnail, setThumbnail] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const onClose = () => setIsOpen(false);
- 
-
   const [videosData, setVideosData] = useState([
     {
       link: '',
@@ -81,7 +70,6 @@ const ControlPanel = () => {
           id: videoId ,
           title: title, 
           link: link, 
-          topic: topic, 
           description: description, 
           thumbnail: thumbnail, 
         }),
@@ -122,70 +110,68 @@ const ControlPanel = () => {
     }
   };
   
-    return ( 
-        <>
-          <Stack spacing={3}>
-            <Input onChange={(e) => setVideoId(e.target.value)} placeholder='id' size='md' />
-            <IconButton onClick={getVideo} aria-label='Search database' icon={<SearchIcon />} />
-            <Tabs variant='enclosed'>
-              <TabList>
-                <Tab>Vlastnosti</Tab>
-                <Tab>Úpravy</Tab>
-              </TabList>
-              <TabPanels>
-              <TabPanel>
-                <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                  <img alt="Thumbnail" src={videosData[0].thumbnail} style={{width: "100%"}} />
-                  <Box p="6">
-                    <Box display="flex" alignItems="baseline">
-                      <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
-                        <Text fontSize="x0.5" fontWeight="bold" color="grey">Název: </Text> 
-                        {videosData[0].title}
-                      </Box>
+  return ( 
+    <>
+      <Stack spacing={3}>
+        <Input onChange={(e) => setVideoId(e.target.value)} placeholder='id' size='md' />
+        <IconButton onClick={getVideo} aria-label='Search database' icon={<SearchIcon />} />
+        <Tabs variant='enclosed'>
+          <TabList>
+            <Tab>Vlastnosti</Tab>
+            <Tab>Úpravy</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+                <img alt="Thumbnail" src={videosData[0].thumbnail} style={{width: "100%"}} />
+                <Box p="6">
+                  <Box display="flex" alignItems="baseline">
+                    <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+                      <Text fontSize="x0.5" fontWeight="bold" color="grey">Název: </Text> 
+                      {videosData[0].title}
                     </Box>
-                    <Divider borderColor="gray.600" my={2}/>
-                    <Box mt="1">
-                      <Text fontSize="x0.5" fontWeight="bold" color="grey">Popis: </Text> 
-                      {videosData[0].description}
-                    </Box>
-                    <Divider borderColor="gray.600" my={2}/>
-                    <Box mt="2">
-                      <Text fontSize="x0.5" fontWeight="bold" color="grey">Odkaz na video: </Text>
-                      <Link href={baseYoutubeUrl + videosData[0].link} isExternal>
-                        {baseYoutubeUrl + videosData[0].link}
-                      </Link>
-                    </Box>
-                    <Divider borderColor="gray.600" my={2}/>
-                    <Box mt="2">
-                      <Text fontSize="x0.5" fontWeight="bold" color="grey">Odkaz na miniaturu: </Text>
-                      <Link href={videosData[0].thumbnail} isExternal>
-                        {videosData[0].thumbnail}
-                      </Link>
-                    </Box>
-                    <Divider borderColor="gray.600" my={2}/>
-                    <Button colorScheme="red" variant="solid" width="full" mt={4} size="sm" boxShadow="x2" onClick={() => setIsOpen(true)}>Vymazat Video</Button>
                   </Box>
+                  <Divider borderColor="gray.600" my={2}/>
+                  <Box mt="1">
+                    <Text fontSize="x0.5" fontWeight="bold" color="grey">Popis: </Text> 
+                    {videosData[0].description}
+                  </Box>
+                  <Divider borderColor="gray.600" my={2}/>
+                  <Box mt="2">
+                    <Text fontSize="x0.5" fontWeight="bold" color="grey">Odkaz na video: </Text>
+                    <Link href={baseYoutubeUrl + videosData[0].link} isExternal>
+                      {baseYoutubeUrl + videosData[0].link}
+                    </Link>
+                  </Box>
+                  <Divider borderColor="gray.600" my={2}/>
+                  <Box mt="2">
+                    <Text fontSize="x0.5" fontWeight="bold" color="grey">Odkaz na miniaturu: </Text>
+                    <Link href={videosData[0].thumbnail} isExternal>
+                      {videosData[0].thumbnail}
+                    </Link>
+                  </Box>
+                  <Divider borderColor="gray.600" my={2}/>
+                  <Button colorScheme="red" variant="solid" width="full" mt={4} size="sm" boxShadow="x2" onClick={() => setIsOpen(true)}>Vymazat Video</Button>
                 </Box>
+              </Box>
                 <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
                   <AlertDialogOverlay>
                     <AlertDialogContent>
-                      <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                        Vymazat Video
-                      </AlertDialogHeader>
-
-                      <AlertDialogBody>
-                        Opravdu chcete vymazat video?
-                      </AlertDialogBody>
-
-                      <AlertDialogFooter>
-                      <Button ref={cancelRef} onClick={onClose}>
-                        Ne
-                      </Button>
-                      <Button colorScheme="red" onClick={deleteVideo} ml={3}>
-                        Ano
-                      </Button>
+                    <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                      Vymazat Video
+                    </AlertDialogHeader>
+                    <AlertDialogBody>
+                      Opravdu chcete vymazat video?
+                    </AlertDialogBody>
+                    <AlertDialogFooter>
+                    <Button ref={cancelRef} onClick={onClose}>
+                      Ne
+                    </Button>
+                    <Button colorScheme="red" onClick={deleteVideo} ml={3}>
+                      Ano
+                    </Button>
                     </AlertDialogFooter>    
-                     </AlertDialogContent>
+                    </AlertDialogContent>
                   </AlertDialogOverlay>
                 </AlertDialog>
                 <ToastContainer
@@ -199,39 +185,39 @@ const ControlPanel = () => {
                   draggable
                   pauseOnHover
                   theme="dark"
-                  />    
-              </TabPanel>
-              <TabPanel>
+                />    
+            </TabPanel>
+            <TabPanel>
               <Text as='b'>Titulek:</Text>
-                <Editable onChange={setTitle} placeholder={videosData[0].title}>
-                    <EditablePreview />
-                    <EditableTextarea />
-                </Editable>
-                <Text as='b'>Popis:</Text>
-                <Editable onChange={setDescription} placeholder={videosData[0].description}>
-                    <EditablePreview />
-                    <EditableTextarea />
-                </Editable>
-                <Text as='b'>Odkaz na video:</Text>
-                <Editable onChange={setLink} placeholder={videosData[0].link}>
-                    <EditablePreview />
-                    <EditableTextarea />
-                </Editable>
-                <Text as='b'>Odkaz na obrźek:</Text>
-                <Editable onChange={setThumbnail} placeholder={placeholderImage}>
-                    <EditablePreview />
-                    <EditableTextarea />
-                </Editable>
-                <br /><Divider orientation='horizontal' /><br />
-                <Button leftIcon={<CheckIcon />} colorScheme='teal' variant='solid' onClick={patchVideo}>
-                  Uložit změny
-                </Button>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </Stack>
-      </>
-    );
+              <Editable onChange={setTitle} placeholder={videosData[0].title}>
+                <EditablePreview />
+                <EditableTextarea />
+              </Editable>
+              <Text as='b'>Popis:</Text>
+              <Editable onChange={setDescription} placeholder={videosData[0].description}>
+                <EditablePreview />
+                <EditableTextarea />
+              </Editable>
+              <Text as='b'>Odkaz na video:</Text>
+              <Editable onChange={setLink} placeholder={videosData[0].link}>
+                <EditablePreview />
+                <EditableTextarea />
+              </Editable>
+              <Text as='b'>Odkaz na obrźek:</Text>
+              <Editable onChange={setThumbnail} placeholder={placeholderImage}>
+                <EditablePreview />
+                <EditableTextarea />
+              </Editable>
+              <br /><Divider orientation='horizontal' /><br />
+              <Button leftIcon={<CheckIcon />} colorScheme='teal' variant='solid' onClick={patchVideo}>
+                Uložit změny
+              </Button>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Stack>
+    </>
+  );
 };
 
 export default ControlPanel;
